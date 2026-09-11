dd4508c234309038b546963db02bdcd90bfddd52
linavlada <avladco@griddynamics.com>
2026-09-11 17:40:23 +0300

    Added health and readiness endpoints

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from database.database import SessionDep
+import asyncio
+from sqlalchemy import text
 
 app = FastAPI()
+
+
+@app.get("/healthz",include_in_schema=False)
+async def health_check():
+    return {"status":"ok"}
+
+@app.get("/readyz",include_in_schema=False)
+async def ready_check(db: SessionDep):
+    try:
+        await asyncio.wait_for(
+            db.execute(text("SELECT 1")),
+            timeout=5.0
+        )
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
